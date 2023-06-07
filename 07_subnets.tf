64d66d0f5b75e68ef4d49be96fb5c6c86e702d2a
Man Sinh Lee <man.sinh.lee@gmail.com>
2023-06-07 08:20:19 +0700

    modify subnets to 3 azs and bash script

--- 07_subnets.tf
+++ 07_subnets.tf
@@ -9,8 +9,9 @@ resource "aws_subnet" "public_subnet" {
   vpc_id                  = aws_vpc.cmcloudlab_vpc.id
   cidr_block              = "${regex("\\d+\\.\\d+", var.vpc_cidr_block)}.${count.index}.0/24"
   map_public_ip_on_launch = true
-  count                   = length(data.aws_availability_zones.available.names)
-  availability_zone       = element(data.aws_availability_zones.available.names, count.index - 3)
+  count                   = 3
+  #count                   = length(data.aws_availability_zones.available.names)
+  availability_zone = element(data.aws_availability_zones.available.names, count.index)
   tags = {
     Name = "public-subnet-${element(data.aws_availability_zones.available.names, count.index)}"
   }
@@ -20,8 +21,9 @@ resource "aws_subnet" "private_subnet" {
   vpc_id                  = aws_vpc.cmcloudlab_vpc.id
   cidr_block              = "${regex("\\d+\\.\\d+", var.vpc_cidr_block)}.${length(data.aws_availability_zones.available.names) + count.index}.0/24"
   map_public_ip_on_launch = false
-  count                   = length(data.aws_availability_zones.available.names - 3)
-  availability_zone       = element(data.aws_availability_zones.available.names, count.index)
+  count                   = 3
+  #count                   = length(data.aws_availability_zones.available.names)
+  availability_zone = element(data.aws_availability_zones.available.names, count.index)
   tags = {
     Name = "private-subnet-${element(data.aws_availability_zones.available.names, count.index)}"
   }
@@ -31,8 +33,9 @@ resource "aws_subnet" "database_subnet" {
   vpc_id                  = aws_vpc.cmcloudlab_vpc.id
   cidr_block              = "${regex("\\d+\\.\\d+", var.vpc_cidr_block)}.${2 * length(data.aws_availability_zones.available.names) + count.index}.0/24"
   map_public_ip_on_launch = false
-  count                   = length(data.aws_availability_zones.available.names - 3)
-  availability_zone       = element(data.aws_availability_zones.available.names, count.index)
+  count                   = 3
+  #count                   = length(data.aws_availability_zones.available.names)
+  availability_zone = element(data.aws_availability_zones.available.names, count.index)
   tags = {
     Name = "database-subnet-${element(data.aws_availability_zones.available.names, count.index)}"
   }
