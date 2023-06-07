d80f85482f7205f2b1b33fb562b4586207875a96
Man Sinh Lee <man.sinh.lee@gmail.com>
2023-06-07 07:46:25 +0700

    update maxsize to 0

--- 32_alb_asc.tf
+++ 32_alb_asc.tf
@@ -1,6 +1,6 @@
 resource "aws_autoscaling_group" "alb_asc" {
   min_size            = 0
-  max_size            = 6
+  max_size            = 0
   desired_capacity    = 0
   vpc_zone_identifier = aws_subnet.private_subnet.*.id
   target_group_arns   = ["${aws_lb_target_group.alb_tg.arn}"]
