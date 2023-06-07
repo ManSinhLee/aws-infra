30dff74ab139b77b1311fabfda295d52d0e6603b
Man Sinh Lee <man.sinh.lee@gmail.com>
2023-06-07 07:53:36 +0700

    update lauch template and asc

--- 26_nlb_asc.tf
+++ 26_nlb_asc.tf
@@ -1,7 +1,7 @@
 resource "aws_autoscaling_group" "nlb_asc" {
-  min_size            = 0
-  max_size            = 0
-  desired_capacity    = 0
+  min_size            = 3
+  max_size            = 6
+  desired_capacity    = 3
   vpc_zone_identifier = aws_subnet.private_subnet.*.id
   target_group_arns   = ["${aws_lb_target_group.nlb_tg.arn}"]
   launch_template {
